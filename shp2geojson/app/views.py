0cf5b56118e3388df016a1fe31a1fc988069de52
Eddie A Tejeda <eddie@codeforamerica.org>
2013-02-13 17:52:23 -0800

    slight better error catching

--- shp2geojson/app/views.py
+++ shp2geojson/app/views.py
@@ -6,6 +6,8 @@ import shutil
 import pprint
 import os.path
 import urlparse
+import json
+import time
 
 from django import forms
 from django.http import HttpResponse
@@ -17,9 +19,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.conf import settings
 
-
-
-# http://geoinformaticstutorial.blogspot.com/2012/10/reprojecting-shapefile-with-gdalogr-and.html
+# 
+# 
+# 
 
 @csrf_exempt
 def index(request):
@@ -31,27 +33,35 @@ def upload_file(request):
 
     uploaded_filename = request.FILES['file'].name
 
-    (filename, extension) = os.path.splitext(uploaded_filename)   
+    (filename, extension) = os.path.splitext(uploaded_filename)
 
     if extension == ".zip":
-      handle_uploaded_file(request.FILES['file'], uploaded_filename)
 
+      handle_uploaded_file(request.FILES['file'], uploaded_filename)
       zipfile_shp = os.path.join(settings.DATA_ROOT, uploaded_filename)
 
-      unzip(zipfile_shp)
+
+      try:
+        unzip(zipfile_shp)
+      except:
+        e = sys.exc_info()[0]
+        return HttpResponse( json.dumps({'error': e}) , content_type="application/json")
 
       fro = os.path.join(settings.DATA_ROOT, filename, filename + '.shp')
       to = os.path.join(settings.APP_STATIC, filename + '.geojson')
-
-      reproject_shp_file_covert_to_geojson( fro, to )
       
-      print to
-      with open(to, 'r') as content_file:
-        geojson_content = content_file.read()
+      print 'to: ' + to
+      try:
+        reproject_shp_file_covert_to_geojson( fro, to )
+        with open(to, 'r') as content_file:
+          geojson_content = content_file.read()
+      except:
+        e = sys.exc_info()[0]
+        geojson_content = json.dumps({'error': e})
+
       return HttpResponse(geojson_content, content_type="application/json")
-      # return redirect()      
     else:
-      return HttpResponse('Please provide zip file')
+      return HttpResponse( json.dumps({'error': 'Please provide zip file'}) , content_type="application/json")
 
   else:
     form = UploadFileForm()
@@ -68,16 +78,20 @@ class UploadFileForm(forms.Form):
   file  = forms.FileField()
 
 
-def reproject_shp_file_covert_to_geojson( fro, to):
+def reproject_shp_file_covert_to_geojson( fro, to, projection = 'EPSG:900913'):
+
   if(os.path.isfile(to)):
     print 'Using cached version of ' + to    
+    print "created: %s" % time.ctime(os.path.getctime(to))
+
   else:
     
+    # print 'OS Enviroment ' + os.environ
     ogr2ogr_path = ''
     if 'PRODUCTION' in os.environ:
       ogr2ogr_path = '/app/.geodjango/gdal/bin/'
 
-    cmd=ogr2ogr_path + 'ogr2ogr -overwrite -F "GeoJSON" -t_srs EPSG:900913 '+to+' ' + fro
+    cmd=ogr2ogr_path + 'ogr2ogr -overwrite -F "GeoJSON" -t_srs '+ projection +' '+to+' ' + fro
     print cmd
     response = os.popen(cmd,"r")
 
